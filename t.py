445d62bac627edf79657b20611a1970fde6caab3
Farruxbek <norpolatovfarruxbek0@gmail.com>
2025-01-09 22:10:39 +0500

    Delete t.py

--- t.py
+++
@@ -1,9 +0,0 @@
-class Taksopark:
-    def __init__(self,title):
-        self.title=title
-        self.balance=0
-        self.cars = []
-        self.safarlar = []
-
-    def add_car(self,car):
-        self.cars.append(car)
